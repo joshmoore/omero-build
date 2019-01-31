ad1a4c9657c8bfbf4cb62aea4c8b9f2d007804da
Jean-Marie Burel <j.burel@dundee.ac.uk>
2019-01-31 21:56:22 +0000

    roll back

--- build.sh
+++ build.sh
@@ -4,18 +4,9 @@ set -e
 set -u
 set -x
 
-for x in omero-model omero-common omero-romio omero-renderer omero-server;
+for x in omero-model omero-common omero-romio omero-renderer omero-server omero-blitz;
 do
     pushd $x
-    ./gradlew clean publishToMavenLocal -x test -x javadoc
-    popd
-done
-
-for x in omero-blitz;
-do
-    pushd $x
-    # stop the daemon since we have to start a new one to honor JVM settings
-    ./gradlew daemon --stop
     ./gradlew clean publishToMavenLocal -x test -x javadoc
     popd
 done
