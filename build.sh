39cc3f962e176fc85202697c3010d5e126b33480
Jean-Marie Burel <j.burel@dundee.ac.uk>
2019-01-31 15:47:34 +0000

    split build

--- build.sh
+++ build.sh
@@ -4,9 +4,18 @@ set -e
 set -u
 set -x
 
-for x in omero-model omero-common omero-romio omero-renderer omero-server omero-blitz;
+for x in omero-model omero-common omero-romio omero-renderer omero-server;
 do
     pushd $x
+    ./gradlew clean publishToMavenLocal -x test -x javadoc
+    popd
+done
+
+for x in omero-blitz;
+do
+    pushd $x
+    # stop the daemon since we have to start a new one to honor JVM settings
+    ./gradlew daemon --stop
     ./gradlew clean publishToMavenLocal -x test -x javadoc
     popd
 done
